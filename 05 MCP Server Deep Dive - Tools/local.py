d191b5fb7b0146208211ee2c032124263fd836a8
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-05-24 19:56:48 +0530

    update the mcp server deep dive tools to read and write the content in the local systems with the custom mcp server

--- 05 MCP Server Deep Dive - Tools/local.py
+++ 05 MCP Server Deep Dive - Tools/local.py
@@ -1,6 +1,12 @@
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("LocalNotes")
+import os
+
+# This always points to the folder where local.py lives
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+NOTES_FILE = os.path.join(BASE_DIR, 'notes.txt')
+
 
 @mcp.tool()
 def add_note_to_file(content: str) -> str:
@@ -10,7 +16,7 @@ def add_note_to_file(content: str) -> str:
         content: The text content to append.
     """
 
-    filename = 'notes.txt'
+    filename = NOTES_FILE
 
     try:
         with open(filename, "a", encoding="utf-8") as f:
@@ -25,7 +31,46 @@ def read_notes() -> str:
     """
     Reads and returns the contents of the user's local notes.
     """
-    filename = 'notes.txt'
+    filename = NOTES_FILE
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            notes = f.read()
+        return notes if notes else "No notes found."
+    except FileNotFoundError:
+        return "No notes file found."
+    except Exception as e:
+        return f"Error reading file {filename}: {e}"
+    
+
+# This always points to the folder where local.py lives
+NOTES_FILE1 = os.path.join(BASE_DIR, 'web.html')
+
+
+@mcp.tool()
+def add_web_note(content: str) -> str:
+    """
+    Appends the given content to the user's local notes.
+    Args:
+        content: The text content to append.
+    """
+
+    filename = NOTES_FILE1
+
+    try:
+        with open(filename, "a", encoding="utf-8") as f:
+            f.write(content + "\n")
+        return f"Content appended to {filename}."
+    except Exception as e:
+        return f"Error appending to file {filename}: {e}"
+    
+
+@mcp.tool()
+def read_web() -> str:
+    """
+    Reads and returns the contents of the user's local notes.
+    """
+    filename = NOTES_FILE1
 
     try:
         with open(filename, "r", encoding="utf-8") as f:
